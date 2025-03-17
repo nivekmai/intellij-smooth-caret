97875edb905b38136038723fe0a554d38281e16a
Vlad Gorokhov <vlad.gorokh@gmail.com>
2025-03-17 03:53:01 +0000

    detect file length changes

--- src/main/kotlin/dev/gorokhov/smoothcaret/SmoothCaretEditorFactoryListener.kt
+++ src/main/kotlin/dev/gorokhov/smoothcaret/SmoothCaretEditorFactoryListener.kt
@@ -3,16 +3,15 @@ package dev.gorokhov.smoothcaret
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.EditorColors
-import com.intellij.openapi.editor.event.EditorFactoryEvent
-import com.intellij.openapi.editor.event.EditorFactoryListener
-import com.intellij.openapi.editor.event.CaretListener
-import com.intellij.openapi.editor.event.CaretEvent
+import com.intellij.openapi.editor.event.*
 import com.intellij.openapi.editor.markup.HighlighterLayer
 import com.intellij.openapi.editor.markup.HighlighterTargetArea
+import com.intellij.openapi.editor.markup.RangeHighlighter
 import java.awt.Color
 
 class SmoothCaretEditorFactoryListener : EditorFactoryListener {
     private val settings = service<SmoothCaretService>().settings
+    private val highlighters = mutableMapOf<Editor, RangeHighlighter>()
 
     override fun editorCreated(event: EditorFactoryEvent) {
         setupSmoothCaret(event.editor)
@@ -42,6 +41,7 @@ class SmoothCaretEditorFactoryListener : EditorFactoryListener {
             null,
             HighlighterTargetArea.EXACT_RANGE
         )
+        highlighters[editor] = highlighter
 
         val renderer = SmoothCaretRenderer(settings)
         highlighter.customRenderer = renderer
@@ -52,19 +52,64 @@ class SmoothCaretEditorFactoryListener : EditorFactoryListener {
             }
         }
 
+        val documentListener = object : DocumentListener {
+            override fun documentChanged(event: DocumentEvent) {
+                val newLength = editor.document.textLength
+                val markupModel = editor.markupModel
+
+                highlighters[editor]?.let { oldHighlighter ->
+                    markupModel.removeHighlighter(oldHighlighter)
+                }
+
+                val newHighlighter = markupModel.addRangeHighlighter(
+                    0,
+                    newLength,
+                    HighlighterLayer.LAST + 1,
+                    null,
+                    HighlighterTargetArea.EXACT_RANGE
+                )
+
+                newHighlighter.customRenderer = highlighters[editor]?.customRenderer
+
+                highlighters[editor] = newHighlighter
+
+                editor.contentComponent.repaint()
+            }
+        }
+
+        editor.document.addDocumentListener(documentListener)
         editor.caretModel.addCaretListener(caretListener)
         editor.putUserData(CARET_LISTENER_KEY, caretListener)
-
-        editor.scrollingModel.addVisibleAreaListener { e ->
-            editor.contentComponent.repaint()
-        }
+        editor.putUserData(DOCUMENT_LISTENER_KEY, documentListener)
     }
 
     override fun editorReleased(event: EditorFactoryEvent) {
+        val editor = event.editor
+
         // Restore default caret color when editor is released
         event.editor.colorsScheme.setColor(EditorColors.CARET_COLOR, null)
+
+        // Remove document listener
+        editor.getUserData(DOCUMENT_LISTENER_KEY)?.let { listener ->
+            editor.document.removeDocumentListener(listener)
+        }
+
+        // Remove caret listener
+        editor.getUserData(CARET_LISTENER_KEY)?.let { listener ->
+            editor.caretModel.removeCaretListener(listener)
+        }
+
+        // Remove highlighter
+        highlighters.remove(editor)?.let { highlighter ->
+            editor.markupModel.removeHighlighter(highlighter)
+        }
+
+        // Restore default caret color
+        editor.colorsScheme.setColor(EditorColors.CARET_COLOR, null)
+
     }
 
 }
 
 private val CARET_LISTENER_KEY = com.intellij.openapi.util.Key<CaretListener>("SMOOTH_CARET_LISTENER")
+private val DOCUMENT_LISTENER_KEY = com.intellij.openapi.util.Key<DocumentListener>("SMOOTH_CARET_DOCUMENT_LISTENER")
